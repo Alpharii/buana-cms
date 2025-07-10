55bd6654dd623a0b09f56dd31936d8a793b82aab
alpharii <alphari404@gmail.com>
2025-07-10 20:26:15 +0700

    chore: change minor post-auth layout, update header title

--- frontend/app/routes/__post-auth.tsx
+++ frontend/app/routes/__post-auth.tsx
@@ -1,9 +1,9 @@
-import { LoaderFunction, redirect } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { json, LoaderFunction, redirect } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { PostauthSidebar } from "~/components/sidebar";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "~/components/ui/sidebar";
 import { tokenCookie } from "./__pre-auth+/login";
-import { setApiToken } from "~/lib/apiClient";
+import { apiClient, setApiToken } from "~/lib/apiClient";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const cookie = request.headers.get("cookie")
@@ -12,17 +12,26 @@ export const loader: LoaderFunction = async ({ request }) => {
   if (!token) return redirect("/login")
 
   setApiToken(token)
-  return null
+  try{
+    const me = await apiClient.get('/users/me')
+    console.log('me', me.data)
+    return json(me.data)
+  } catch(error){
+    console.log('err', error)
+    return null
+  }
 }
 
 export default function PostauthLayout() {
+  const data: any = useLoaderData()
+  console.log('personal data',data)
   return (
     <SidebarProvider>
       <PostauthSidebar />
       <SidebarInset>
         <header className="flex h-16 items-center border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <h1 className="ml-4 text-lg font-semibold">Dashboard</h1>
+          <h1 className="ml-4 text-lg font-semibold">Buana CMS</h1>
         </header>
         <main className="p-4">
           <Outlet />
